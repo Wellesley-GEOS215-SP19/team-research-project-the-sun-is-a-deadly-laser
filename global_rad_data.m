67cbdc561c71fd50bce40945af766e8b310be71f
Leafia <lsherade@wellesley.edu>
2019-04-07 01:39:32 -0400

    finally got station data and global radiation data into variables

---
+++ global_rad_data.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ea85e89922e3a6a655528e104594c1dec1ba68aaaa88adc1193f41b96a7c517
+size 799
---
+++ midas_station_data.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f700322e20f2aa0102ab9fa8e9981a93d344ee3b2b44dd411ea2a8eee8daf68b
+size 688
