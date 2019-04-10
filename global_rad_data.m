04d3b89d55af5b53867be1bfd4691627ff621308
Leafia <lsherade@wellesley.edu>
2019-04-10 14:15:07 -0400

    working

--- global_rad_data.m
+++ global_rad_data.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2260128872012396d7bbbe014f635e4b6bde351f9a1886319c42c983587b3b2e
-size 1902
+oid sha256:d5ab35f0bb742f530f00cf62655582ea7f1ed9a89d63bec461fa98b7c3e4364f
+size 1524
--- station_data.m
+++ station_data.m
@@ -1,14 +1,20 @@
+% Jocelyn and Leafia
+%%
+%MIDAS station data
+
+%%
 %tabular datastore for all the stations
-station_ttds = tabularTextDatastore('/Users/leafiasheradencox/Desktop/GEOS 215/GitHub/midas_stations_by_area');
+station_ttds = tabularTextDatastore('/Users/leafiasheradencox/Desktop/GEOS 215/GitHub/team-research-project-the-sun-is-a-deadly-laser/midas_stations_by_area');
 %station_ttds.ReadVariableNames = 1
 station_ttds.NumHeaderLines = 1;
 station_ttds.VariableNames = {'X','Y','Name','description','snippet','end_date','start_date','src_id','Area','Postcode'};
-station_ttds.TextscanFormats = {'%f','%f','%q','%*q','%*q','%*q','%q','%f','%*q','%*q'};
+station_ttds.TextscanFormats = {'%f','%f','%q','%q','%q','%q','%q','%f','%q','%q'};
 station_ttds.SelectedVariableNames = {'X', 'Y','src_id'};
 
 %%
-station_ids = station_ttds.readall;
-
-%stationdata = readall(station_ttds)
+%station_ids = station_ttds.readall;
+station_ids = readall(station_ttds);
 %%
 save('station_ids.mat','station_ids');
+
+
